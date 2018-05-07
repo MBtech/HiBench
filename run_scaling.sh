9124ae186d788d79c858f08a90e97ad6905ead61
MBtech <mubil@kth.se>
2018-05-07 12:00:17 +0000

    updated conf and scripts

--- run_scaling.sh
+++ run_scaling.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sed -i "s/hibench.hdfs.master.*/hibench.hdfs.master       hdfs:\/\/$(hostname --ip-address):9000/g" conf/hadoop.conf 
+
+sed -i "s/hibench.spark.master.*/hibench.spark.master spark:\/\/$(hostname --ip-address):7077/g" conf/spark.conf
+
 sizes=(tiny small large huge gigantic bigdata)
 for size in ${sizes[@]}; do
 
