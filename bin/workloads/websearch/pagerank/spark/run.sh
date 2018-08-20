366730211f46d48fb220d69b4ba68cc9d69de749
MBTech <mubil@kth.se>
2018-08-20 06:35:32 +0000

    Bayes, GBT and PageRank verified

--- bin/workloads/websearch/pagerank/spark/run.sh
+++ bin/workloads/websearch/pagerank/spark/run.sh
@@ -29,7 +29,7 @@ SIZE=`dir_size $INPUT_HDFS`
 START_TIME=`timestamp`
 ssh -t ubuntu@$CASSANDRA_IP "cqlsh $CASSANDRA_IP -e \"DROP TABLE IF EXISTS test.pagerank;\""
 ssh -t ubuntu@$CASSANDRA_IP "cqlsh $CASSANDRA_IP -e \"CREATE KEYSPACE IF NOT EXISTS test WITH REPLICATION = {'class' : 'SimpleStrategy','replication_factor' : 1};\""
-ssh -t ubuntu@$CASSANDRA_IP "cqlsh $CASSANDRA_IP -e \"CREATE TABLE IF NOT EXISTS test.pagerank (url varchar, rank float, PRIMARY KEY (url) );\""
+ssh -t ubuntu@$CASSANDRA_IP "cqlsh $CASSANDRA_IP -e \"CREATE TABLE IF NOT EXISTS test.pagerank (id bigint, rank float, PRIMARY KEY (id) );\""
 run_spark_job org.apache.spark.examples.SparkPageRank $INPUT_HDFS/edges $OUTPUT_HDFS $NUM_ITERATIONS
 END_TIME=`timestamp`
 
